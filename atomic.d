423d2fbc026c1e0f1a8a95002ceb5cc524c9b322
rymrg <54061433+rymrg@users.noreply.github.com>
2021-07-21 21:47:57 +0300

    Pointers

--- atomic.d
+++ atomic.d
@@ -2,14 +2,14 @@ module drm.atomic;
 public import core.atomic : MemoryOrder;
 
 /// Atomic data like std::atomic
-struct Atomic(T) if (__traits(isIntegral, T)){
+struct Atomic(T) if (__traits(isIntegral, T) || isPointer!T){
 	import core.atomic : atomicLoad, atomicStore, atomicExchange, atomicFetchAdd,
 		   atomicFetchSub, atomicCas = cas, atomicCasWeak = casWeak, atomicOp;
 
     private shared T val;
 
 	/// Constructor
-	this(T init){
+	this(T init) shared{
 		val.atomicStore(init);
 	}
 
@@ -21,7 +21,7 @@ struct Atomic(T) if (__traits(isIntegral, T)){
 	alias load this;
 
 	/// ditto
-    T load(MemoryOrder mo = MemoryOrder.seq)() shared const {
+    T load(MemoryOrder mo = MemoryOrder.seq)() shared {
         return val.atomicLoad!mo;
     }
 
@@ -55,9 +55,9 @@ struct Atomic(T) if (__traits(isIntegral, T)){
     }
 
 	/// ditto
-    //bool casWeak(MemoryOrder mo = MemoryOrder.seq, MemoryOrder fmo = MemoryOrder.seq)(T oldVal, T newVal) shared {
-    //    return atomicCasWeak!(mo, fmo)(ptr, oldVal, newVal);
-    //}
+    bool casWeak(MemoryOrder mo = MemoryOrder.seq, MemoryOrder fmo = MemoryOrder.seq)(T oldVal, T newVal) shared {
+        return atomicCasWeak!(mo, fmo)(ptr, oldVal, newVal);
+    }
     
 	/// Op assign with SC semantics
     T opOpAssign(string op)(T rhs) shared {
@@ -65,8 +65,7 @@ struct Atomic(T) if (__traits(isIntegral, T)){
     }
     
 	/// Implicit conversion to FADD and FSUB
-    T opUnary(string op)() shared
-    {
+    T opUnary(string op)() shared {
         static if (op == `++`)
             return val.atomicOp!`+=`(1);
         static if (op == `--`)
@@ -80,7 +79,7 @@ struct Atomic(T) if (__traits(isIntegral, T)){
 	assert(a.load == 0);
 	assert(a.fadd!(MemoryOrder.raw)(5) == 0);
 	assert(a.load!(MemoryOrder.acq) == 5);
-	//assert(!a.casWeak(4,5));
+	assert(!a.casWeak(4,5));
 	assert(!a.cas(4,5));
 	assert(a.cas!(MemoryOrder.rel, MemoryOrder.acq)(5,4));
 	assert(a.fsub!(MemoryOrder.acq_rel)(2) == 4);
@@ -124,4 +123,25 @@ unittest{
 	t1.join;
 }
 
-enum bool isPointer(T) = is(T == U*, U) && !isAggregateType!T;
+@safe unittest{
+	shared Atomic!(shared(int)) a = 5;
+	assert(a.load == shared(int)(5));
+	a = 2;
+	assert(a == 2);
+}
+@safe unittest{
+	shared Atomic!(shared(int)*) ptr = new shared(int);
+	*ptr.load!(MemoryOrder.raw)() = 5;
+	assert(*ptr.load == 5);
+	*(ptr.load) = 42;
+	assert(*ptr.load == 42);
+}
+
+unittest{
+	//Atomic!(shared(Atomic!(int))*) ptr = new shared(Atomic!int);
+}
+
+
+private enum bool isAggregateType(T) = is(T == struct) || is(T == union) ||
+                               is(T == class) || is(T == interface);
+private enum bool isPointer(T) = is(T == U*, U) && !isAggregateType!T;
